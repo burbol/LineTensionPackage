a4b7476ecf820a98e9810dbe64b9f6e15269ce53
Laila Eixeres <burbol2@Lailas-MacBookPro.local>
2016-01-29 11:42:36 +0100

    Finished package pdbfile2.py for new structure

--- SYSTEM_CREATION/pdbfile2.py
+++ SYSTEM_CREATION/pdbfile2.py
@@ -14,13 +14,6 @@ Wy = a0*np.cos(np.pi/6)
 
 ################# Head Groups  ##################
 
-
-####  SAMs Head Groups  TOP ####
-
-xLengthCHTOP = -0.90
-yLengthCHTOP = 0.00
-zLengthCHTOP = +0.41
-
 ####  OAM Head Groups  TOP ####
 
 xLengthCO = 0.22
@@ -29,12 +22,32 @@ zLengthCO = -1.42
 
 xLengthOH = +0.87
 yLengthOH = 0.00
-zLengthOH = +0.39    
+zLengthOH = +0.39
 
 ############## SAM & OAM CHAINS ##############
 xLengthCCa = -1.40
 yLengthCCa = 0.00
 zLengthCCa = -0.64
+
+xLengthCCb = +0.15
+yLengthCCb = 0.00
+zLengthCCb = -1.53
+
+
+""" Not needed anymore
+
+####  SAMs Head Groups  TOP ####
+
+xLengthCHTOP = -0.90
+yLengthCHTOP = 0.00
+zLengthCHTOP = +0.41
+
+####  Head Groups  BOTTOM  (a) ####
+
+xLengthCHBOTTOM = 0.10
+yLengthCHBOTTOM = 0.00
+zLengthCHBOTTOM = -0.99
+
 
 xLengthCH1a = -0.50
 yLengthCH1a = +0.81
@@ -44,10 +57,6 @@ xLengthCH2a = -0.50
 yLengthCH2a = -0.810
 zLengthCH2a = +0.29
 
-xLengthCCb = +0.15
-yLengthCCb = 0.00
-zLengthCCb = -1.53
-
 xLengthCH1b = +0.50
 yLengthCH1b = +0.81
 zLengthCH1b = -0.29
@@ -55,31 +64,25 @@ zLengthCH1b = -0.29
 xLengthCH2b = +0.50
 yLengthCH2b = -0.810
 zLengthCH2b = -0.29
-
-####  Head Groups  BOTTOM  (a) ####
-
-xLengthCHBOTTOM = 0.10
-yLengthCHBOTTOM = 0.00
-zLengthCHBOTTOM = -0.99
+"""
 
 
 ################# Functions  ##################
 
+################# New Functions  ##################
 
-################# Write atoms  ##################
-
-
-def writeC(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC):
-    atomtype = 'C'
-    atomnum=str(atomtype)+str(indexC)
-    printline(totalpos, atomnum, chaintype, chainNum,xoldC,yoldC,zoldC) # C
+""" OH head group """  
+def writeO(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexO): 
+    atomtype = 'O'
+    atomnum=str(atomtype)+str(indexO)
+    printline(totalpos, atomnum, chaintype, chainNum, xoldC,yoldC,zoldC)  # O
     xnew = xoldC + xLength
     ynew = yoldC + yLength
     znew = zoldC + zLength
     totalpos = totalpos + 1
-    indexC = indexC + 1
-    return xnew,ynew,znew,totalpos,indexC
-    
+    indexC = indexO + 1
+    return xnew,ynew,znew,totalpos,indexO
+
 def writeH(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH):
     atomtype = 'H'
     atomnum=str(atomtype)+str(indexH)
@@ -91,20 +94,6 @@ def writeH(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos
     indexH = indexH + 1
     return totalpos,indexH
     
-def writeO(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexO): 
-    atomtype = 'O'
-    atomnum=str(atomtype)+str(indexO)
-    printline(totalpos, atomnum, chaintype, chainNum, xoldC,yoldC,zoldC)  # O
-    xnew = xoldC + xLength
-    ynew = yoldC + yLength
-    znew = zoldC + zLength
-    totalpos = totalpos + 1
-    indexC = indexO + 1
-    return xnew,ynew,znew,totalpos,indexO
-    
-    
-################# Write molecules  ##################
-
 def writeOH(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexO,indexH):
     ####### OXYGEN ######
     xnew,ynew,znew,totalpos,indexO = writeO(xLengthCO,yLengthCO,zLengthCO,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexO)
@@ -112,56 +101,69 @@ def writeOH(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexO,indexH):
     totalpos,indexH = writeH(xLengthOH,yLengthOH,zLengthOH,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
     
     return xnew,ynew,znew,totalpos,indexO,indexH
+
+""" For CH2 and CH3"""    
+def writeC(xLength,yLength,zLength,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC):
+    atomtype = 'CHA'
+    atomnum=str(atomtype)+str(indexC)
+    printline(totalpos, atomnum, chaintype, chainNum,xoldC,yoldC,zoldC) # C
+    xnew = xoldC + xLength
+    ynew = yoldC + yLength
+    znew = zoldC + zLength
+    totalpos = totalpos + 1
+    indexC = indexC + 1
+    return xnew,ynew,znew,totalpos,indexC
+   
+        
+def writeCHa(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
     
+    ####### CARBON ######
+    xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCb,yLengthCCb,zLengthCCb,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
+    """ Not needed anymore
+    ####### H1 ######
+    totalpos,indexH = writeH(xLengthCH1a,yLengthCH1a,zLengthCH1a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
+    #Second H atom (H2)
+    totalpos,indexH = writeH(xLengthCH2a,yLengthCH2a,zLengthCH2a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
+    """       
+    return xnew,ynew,znew,totalpos,indexC,indexH
+   
+   
+def writeCHb(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
+    
+    ####### CARBON ######
+    xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCa,yLengthCCa,zLengthCCa,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
+    """ Not needed anymore
+    ####### H1 ######
+    totalpos,indexH = writeH(xLengthCH1b,yLengthCH1b,zLengthCH1b,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
+    ####### H2 ######
+    totalpos,indexH = writeH(xLengthCH2b,yLengthCH2b,zLengthCH2b,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
+    """     
+    return xnew,ynew,znew,totalpos,indexC,indexH
     
 def writeCHTOP(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
     
     ####### CARBON ######
     xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCb,yLengthCCb,zLengthCCb,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
+    """ Not needed anymore
     ####### H1 ######
     totalpos,indexH = writeH(xLengthCH1b,yLengthCH1a,zLengthCH1a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
     ####### H2 ######
     totalpos,indexH = writeH(xLengthCH2b,yLengthCH2a,zLengthCH2a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
     ####### H3 ######
     totalpos,indexH = writeH(xLengthCHTOP,yLengthCHTOP,zLengthCHTOP,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-            
+    """      
     return xnew,ynew,znew,totalpos,indexC,indexH
-    
-
-def writeCHa(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
-    
-    ####### CARBON ######
-    xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCb,yLengthCCb,zLengthCCb,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
-    ####### H1 ######
-    totalpos,indexH = writeH(xLengthCH1a,yLengthCH1a,zLengthCH1a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-    #Second H atom (H2)
-    totalpos,indexH = writeH(xLengthCH2a,yLengthCH2a,zLengthCH2a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-            
-    return xnew,ynew,znew,totalpos,indexC,indexH
-   
-        
-def writeCHb(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
-    
-    ####### CARBON ######
-    xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCa,yLengthCCa,zLengthCCa,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
-    ####### H1 ######
-    totalpos,indexH = writeH(xLengthCH1b,yLengthCH1b,zLengthCH1b,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-    ####### H2 ######
-    totalpos,indexH = writeH(xLengthCH2b,yLengthCH2b,zLengthCH2b,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-            
-    return xnew,ynew,znew,totalpos,indexC,indexH
-
 
 def writeCHBOTTOM(chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC,indexH):
     
     ####### CARBON ######
     xnew,ynew,znew,totalpos,indexC = writeC(xLengthCCb,yLengthCCb,zLengthCCb,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexC)
+    """ Not needed anymore
     ####### H1 ######
     totalpos,indexH = writeH(xLengthCH1a,yLengthCH1a,zLengthCH1a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
     ####### H2 ######
     totalpos,indexH = writeH(xLengthCH2a,yLengthCH2a,zLengthCH2a,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
     ####### H3 ######
     totalpos,indexH = writeH(xLengthCHBOTTOM,yLengthCHBOTTOM,zLengthCHBOTTOM,chaintype,chainNum,xoldC,yoldC,zoldC,totalpos,indexH)
-                        
+    """                  
     return xnew,ynew,znew,totalpos,indexC,indexH
-
