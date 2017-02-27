a84bcbcaa82960a2eb3ffb1f9861099bbce101c3
burbol <eixeres@imac-eixeres.physik.fu-berlin.de>
2017-02-27 21:46:19 +0100

    Modified script to extend simulations
    Still has to be done:
    create the submission scripts and submit them!!

--- Tools/create_yoshi_gpu_jobscripts.py
+++ Tools/create_yoshi_gpu_jobscripts.py
@@ -6,6 +6,8 @@ import os
 import math
 import fnmatch # for filtering the file list to get the top,gro,mdp,ndx files.
 
+# If the simulation needs to be extended there is no need for equilibration or for running "grompp"
+Extend = 'yes' # use 'yes' or 'no'
 
 N = 5  # NUMBER OF SCRIPTS TO SUBMIT = total time running will be this number times the Walltime
 
@@ -14,33 +16,21 @@ SimTime = 120   # maximum run length of simulation in hours, it can be also a fr
 
 Email = 'laila.e@fu-berlin.de'
 
-#NodesNum =  2 
 Cpus = 8  # soroban = 12 cpus/node ; sheldon-ng = 8 cpus/node
 MemPerCpu = 1024
-#SimLen = 60000  # total simulation time length in ps
 Partition = 'gpu-main' # use 'test' for testing and 'main' otherwise 
 
-#next 3 lines only useful when running also grompp
-#IndexFile = ' 'mpi for one node
-#FirstGro = 'sam17_water216'
-#FirstMdp = 'Mini.mdp'
+Simns = {216:200, 1000:200, 2000:200, 3000:200, 4000:200, 5000:200, 6500:200, 7000:200, 8000:200, 9000:200, 10000:200}  # total simulation time length in ns
+Nodes = {1000:1, 2000:1, 3000:1, 4000:1, 5000:1, 6500:1, 7000:1, 8000:1, 9000:1, 10000:1}
+
+pc = [11]  # For testing
+molec = [9000] #For testing
+
+#pc = [0, 11, 22, 33, 37, 44, 50]
+#molec = [1000, 2000, 3000, 4000, 5000, 6500, 7000, 8000, 9000, 10000]
 
 
-Simns = {216:60, 1000:60, 2000:60, 3000:60, 4000:60, 5000:60, 6500:60, 7000:80, 8000:80, 9000:80, 10000:100}  # total simulation time length in ns
-Nodes = {1000:1, 2000:1, 3000:1, 4000:1, 5000:1, 6500:1, 7000:1, 8000:1, 9000:1, 10000:1}  # 1 node
-#Nodes = {1000:2, 2000:2, 3000:2, 4000:2, 5000:2, 6500:2, 7000:2, 8000:2, 9000:2, 10000:2} # 2 nodes
-# old Nodes = {216:2, 1000:3, 2000:3, 3000:3, 4000:3, 5000:3, 6500:3, 7000:5, 8000:6, 9000:6, 10000:6} 
-
-#pc = [11]  # For testing
-#molec = [9000] #For testing
-
-pc = [0, 11, 22, 33, 37, 44, 50]
-molec = [1000, 2000, 3000, 4000, 5000, 6500, 7000, 8000, 9000, 10000]
-
-
-BackupDir = '/home/eixeres/Version_v2/FINISHED/'   # directory to copy (backup) simulation output
-# old BackupDir = '/home/eixeres/Dec14_Last_Sims/FINISHED/'==>>> mpi for one node
-
+BackupDir = '/home/eixeres/Version_v2/FINISHED_extended/'   # directory to copy (backup) simulation output
 
 for i in pc:
 	for j in molec:
@@ -48,34 +38,29 @@ for i in pc:
 		SimLen = Simns[j]*1000  # total simulation time length in ps
 		NodesNum = Nodes[j]
 		CpuNum = NodesNum*Cpus
-		#N = Simns[j]  # NUMBER OF SCRIPTS TO SUBMIT
-		
-		# old Dir = '/Volumes/Backup/YosemiteFiles/MEGAsync/scripts/Python/SCRIPT_CREATION/sheldon-ng/s'+str(i)+'_w'+str(j)
-		#Dir = '/Volumes/Backup/YosemiteFiles/Dropbox/Dropbox/scripts/Python/SCRIPT_CREATION/sheldon-ng/s'+str(i)+'_w'+str(j)+'/1node'
-		#Dir = '/home/eixeres/Downloads/SCRIPTS_JOBS_NewVersion4/' + str(NodesNum) + 'node'+'/s'+str(i)+'_w'+str(j)
-		#Dir = '/Users/burbol2/Downloads/SCRIPTS_JOBS_NewVersion4/' + str(NodesNum) + 'node'+'/s'+str(i)+'_w'+str(j)
-		#Dir = '/Users/eixeres/Dropbox/GitHub/LineTensionPackage/Tools/SubmissionScripts/' + str(NodesNum) + 'node'+'/s'+str(i)+'_w'+str(j)
+
 		DirNode = '/Users/eixeres/Dropbox/GitHub/LineTensionPackage/Tools/SubmissionScripts/' + str(NodesNum) + 'node' # main local directory where scripts using same number of nodes will be saved 
+		DirNode = '/Users/eixeres/Desktop'
 
 		if not os.path.exists(DirNode):
 			os.system("mkdir "+ DirNode)
 		Dir = DirNode +'/s'+str(i)+'_w'+str(j)
 		
 		SimDir = '/scratch/eixeres/Version_v2/s'+str(i)+'_w'+str(j)   #directory from where to run simulation
-		ScriptDir = '/scratch/eixeres/Version_v2/scripts/'+str(i)+'_w'+str(j)+'/' # remote directory with submission scripts (forward slash at the end!!)
-		Filename = 'NVT_sam'+str(i)+'_water'+str(j)  # name of files #===>>>> WHY 2 DIFF. VARIABLES?? Filename and NVTfile??
-		Scriptname = str(NodesNum) + 'n_s'+str(i)+'_w'+str(j)+'_gpu' # name of script (when NOT testing, set equal to "Filename")??
+		ScriptDir = '/scratch/eixeres/Version_v2/scripts/s'+str(i)+'_w'+str(j)+'/' # remote directory with submission scripts (forward slash at the end!!)
+		Filename = 'NVT_sam'+str(i)+'_water'+str(j)  # name of files 
+		Scriptname ='s'+str(i)+'_w'+str(j)+'_gpu'  # name of script
 		
 		Startfile='sam'+str(i)+'_water'+str(j)
 		Minifile='Mini_sam'+str(i)+'_water'+str(j)
-		NVTfile = 'NVT_sam'+str(i)+'_water'+str(j)
+		#NVTfile = 'NVT_sam'+str(i)+'_water'+str(j) #since we run only NVT we don't need a different name for NVT and NPT simulation, we use directly "Filename"
 		topfile= str(i)+'pc_'+str(j)+'.top'
 		Minimdp = 'Mini_v2.mdp'
 		NVTmdp = 'NVT_'+str(Simns[j])+'ns_v2.mdp'
-
+	
 		if not os.path.exists(Dir):
 			os.system("mkdir "+ Dir)	
-
+			
 		for k in range(N):
 		
 			#Jobname = str(NodesNum) +'n_s'+str(i)+'_w'+str(int(j/1000))+ '_'+str(k) +'gpu' # Name of job
@@ -114,30 +99,34 @@ for i in pc:
 			JobOut.write('\n')
 
 			if k == 0:
-				JobOut.write('gmx grompp -f ' + Minimdp + ' -c ' + Startfile + '.gro -p ' + topfile + ' -o '+ Minifile +'.tpr -maxwarn 9\n')
-				
-				# For more then 1 node use mpirun
-				#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -deffnm ' + Minifile + ' -maxh ' + str(SimTime) + ' -v \n')
 
-				# For only 1 node mpi not needed:
-				JobOut.write(' mdrun_gpu -deffnm ' + Minifile + ' -maxh ' + str(SimTime) + ' -v \n')
+				if Extend == 'yes':
+					#first jobscript changes "mdp" options with tpbconv to extend simulation
+					JobOut.write('gmx convert-tpr -s ' + Filename + '.tpr -until ' + str(SimLen) + ' -o ' + Filename + '.tpr \n')
 				
-				JobOut.write('gmx grompp -f ' + NVTmdp + ' -c ' + Minifile + '.gro -p ' + topfile + ' -o '+ NVTfile +'.tpr -maxwarn 9\n')
+				elif Extend == 'no':
+					JobOut.write('gmx grompp -f ' + Minimdp + ' -c ' + Startfile + '.gro -p ' + topfile + ' -o '+ Minifile +'.tpr -maxwarn 9\n')
+				
+					# For more then 1 node use mpirun (check command name for gpu!!!!!!!!!)
+					#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -deffnm ' + Minifile + ' -maxh ' + str(SimTime) + ' -v \n')
 
-				#first jobscript changes "mdp" options with tpbconv to extend simulation
-				#JobOut.write('tpbconv -s ' + Filename + '.tpr  -until ' + str(SimLen) + ' -o ' + Filename + '.tpr \n')
+					# For only 1 node mpi not needed:
+					JobOut.write('mdrun_gpu -deffnm ' + Minifile + ' -maxh ' + str(SimTime) + ' -v \n')
 				
+					JobOut.write('gmx grompp -f ' + NVTmdp + ' -c ' + Minifile + '.gro -p ' + topfile + ' -o '+ Filename +'.tpr -maxwarn 9\n')
+
 				JobOut.write('\n')
 				
 				# For more then 1 node use mpirun
-				#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -s ' + Filename + '.tpr -deffnm  '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
+				#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -s ' + Filename + '.tpr -deffnm '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
 				
 				# For only 1 node mpi not needed:
-				JobOut.write(' mdrun_gpu -s ' + Filename + '.tpr -deffnm  '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
+				JobOut.write('mdrun_gpu -s ' + Filename + '.tpr -deffnm '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
 
 			else:
-				#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -cpi '+ Filename + '.cpt -s ' + Filename + '.tpr -deffnm  '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
-				JobOut.write('mdrun_gpu -cpi '+ Filename + '.cpt -s ' + Filename + '.tpr -deffnm  '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
+				# For more then 1 node use mpirun
+				#JobOut.write('mpirun -np ' + str(CpuNum) + ' mdrun -cpi '+ Filename + '.cpt -s ' + Filename + '.tpr -deffnm '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
+				JobOut.write('mdrun_gpu -cpi '+ Filename + '.cpt -s ' + Filename + '.tpr -deffnm '+ Filename + ' -maxh ' + str(SimTime) + ' -v \n')
 
 			JobOut.write('\n')	
 			
@@ -156,6 +145,3 @@ for i in pc:
 				JobOut.write('cp -r ' + SimDir + ' ' + BackupDir + '\n')
 				
 			JobOut.close()
-
-
-
